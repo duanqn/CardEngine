b359391361781b627e42a6d4822518272805cef5
Qingnan Duan <duanqn_own_1@yeah.net>
2020-10-07 22:20:56 +0800

    Add source code files

---
+++ CardEngine/CardEngine.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include "const.h"
+#include "card.pb.h"
+
+int main()
+{
+    JoinRequest req;
+    req.set_version(VERSION_LATEST);
+    std::cout << "Hello World!\n";
+    return 0;
+}
---
+++ CardEngine/const.h
@@ -0,0 +1,3 @@
+#pragma once
+#include <cstdint>
+const uint32_t VERSION_LATEST = 1;
